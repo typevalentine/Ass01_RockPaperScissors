e6b1004dc2b12dcb67269dfe18994d2e1edc637d
typevalentine <koilui7112@gmail.com>
2024-09-26 16:04:29 -0400

    Final

--- src/Main.java
+++ src/Main.java
@@ -62,54 +62,5 @@ public class Main {
         }
         else
             System.out.println("Invalid move! Run the program again!");
-
     }
 }
-
-
-
-
-// RockPaperScissors
-//    Main()
-//        String play1 = ""
-//        String play2 = ""
-//
-//        do
-//            do
-//            output "Player 1 enter your move [RPS]: "
-//            input play 1
-//            output "Player 2 enter your move [RPS]: "
-//            input play 2
-//            while(play1 !"R","P","S" or play2 !"R","P","S")
-//
-//        if play1 == "R" then
-//            if play2 == "R" then
-//                output "Rock vs Rock, It's a TIE!"
-//            elseif play2 == "P" then
-//                output "Paper covers Rock, Player 2 wins!"
-//            else // play 2 is Scissors
-//                 output "Rock breaks Scissors, Player 1 wins!"
-//            endIf
-//        elseif play1 == "P" then
-//             if play2 == "R" then
-//                output "Paper covers Rock, Player 1 wins!"
-//            elseif play2 == "P" then
-//                output "Paper vs Paper, It's a TIE!"
-//            else // play 2 is Scissors
-//                 output "Scissors cuts paper, Player 2 wins!"
-//            endIf
-//        else // play1 is Scissors
-//             if play2 == "R" then
-//                output "Rock breaks Scissors, Player 2 wins!"
-//            elseif play2 == "P" then
-//                output "Scissors cuts Paper, Player 1 wins!"
-//            else // play 2 is Scissors
-//                 output "Scissors vs Scissors, It's a TIE!"
-//            endIf
-//        Output "Wanna play again [YN]?: "
-//
-//        while(!N)
-//
-//        Output "Thanks for playing!!"
-//    return
-//endClass
